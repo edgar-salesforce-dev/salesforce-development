6b5f00f43ca2c6456dcb02f65f70f63e26230535
edgar-salesforce-dev <sr.sherlock@yahoo.com>
2025-09-19 21:28:06 -0600

    Base Components: lightning-accordion & lightning-accordion-section

--- force-app/main/default/lwc/testBaseComponents/testBaseComponents.js
+++ force-app/main/default/lwc/testBaseComponents/testBaseComponents.js
@@ -1,6 +1,10 @@
 import { LightningElement } from 'lwc';
+import getRecords from '@salesforce/apex/BaseComponentsController.getRecords';
 
-const INVALID_PROPS = [ 'subpremise', 'validity' ]
+const INVALID_PROPS = [ 'subpremise', 'validity' ];
+
+const SECTIONS_OBJECTS = [ 'Accounts', 'Contacts', 'Opportunities' ];
+const VALID_FIELD_TYPES = [ 'Phone', 'Email' ];
 
 export default class TestBaseComponents extends LightningElement {
     // Location
@@ -33,5 +37,54 @@ export default class TestBaseComponents extends LightningElement {
 
             this[key] = details[key];
         }
+    }
+
+    /**
+     * CONNECTED CALLBACK Method
+     */
+    connectedCallback() {
+        this.handleGetObjectData();
+    }
+
+    /**
+     * Accordion & Accordion Section
+     */
+    isLoadingAccordion = true;
+    sections = [];
+    activeSection = SECTIONS_OBJECTS[0];
+
+    handleSectionToggle(event) {
+        this.activeSection = event.detail.openSection;
+    }
+
+    async handleGetObjectData() {
+        const sections = [];
+
+        for (const obj of SECTIONS_OBJECTS) {
+            const data = await getRecords({ objects: obj });
+            
+            const cols = [];
+            
+            for (const field in data[0]) {
+                if (field === 'Id') continue;
+                const col = {
+                    label: field,
+                    fieldName: field,
+                    type: VALID_FIELD_TYPES.includes(field) ? field.toLowerCase() : 'text'
+                }
+                cols.push(col);
+            }
+            
+            const section = {
+                name: obj,
+                data: data,
+                cols: cols
+            }
+
+            sections.push(section);
+        }
+
+        this.sections = sections;
+        this.isLoadingAccordion = false;
     }
 }
